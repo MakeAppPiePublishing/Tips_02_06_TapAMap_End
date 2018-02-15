437349f4fc77131c8e33de56ee7657fc8a683dbb
Steve Lipton <steve@makeapppie.com>
2018-02-15 17:08:40 -0600

    Initial Commit

---
+++ MapTouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapTouch
+//
+//  Created by Steven Lipton on 2/15/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
