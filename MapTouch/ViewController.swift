e56d02839475f874285675c58f9511a23a96a73a
Steve Lipton <stevelipton@brk-16892.local>
2018-02-27 08:30:53 -0800

    initial commit

--- MapTouch/ViewController.swift
+++ MapTouch/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,MKMapViewDelegate {
+    //Two Rivers WI, the likely home of the Ice cream sundae
+    let baseCoordinate = CLLocationCoordinate2DMake(44.1487225,-87.5684114)
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func tapGuesture(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended{
+            let locationInView = sender.location(in: mapView)
+            let tappedCoordinate = mapView.convert(locationInView, toCoordinateFrom: mapView)
+            addAnnotation(coordinate: tappedCoordinate)
+        }
+    }
+    @IBAction func clearAnnotations(_ sender: UIButton) {
+        mapView.removeAnnotations(mapView.annotations)
+    }
+    
+    func addAnnotation(coordinate:CLLocationCoordinate2D){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+    }
+    func setupMap(){
+        let region = MKCoordinateRegionMakeWithDistance(baseCoordinate, 500, 500)
+        mapView.region = region
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupMap()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
